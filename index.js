165bf8bf7a7ad7ee3398c15d817b843595073be5
Jordan Laird <jordanmack.laird@gmail.com>
2018-09-25 15:43:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function takeANumber(katzDeliLine,name) {
+  katzDeliLine.push(name)
+  
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(deliLine) {
+  var currentlyServing = []
+  let i = 0;
+  while (i < deliLine.length ) {
+    i++;
+  }
+  if (deliLine === 0) {
+    currentlyServing.push('There is no one waiting to be served');
+    return currentlyServing;
+  }
+  else
+    currentlyServing.push(`Currently serving ${deliLine.shift()}`)
+    return currentlyServing;
+}
+
+
